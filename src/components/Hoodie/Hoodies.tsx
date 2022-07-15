d37cf037a4e690ad2276d7241f90a53c4a2ed487
gewalttat <rbhorses@mail.ru>
2022-07-15 19:34:55 +0500

    wp goods desk update

--- src/components/Hoodie/Hoodies.tsx
+++ src/components/Hoodie/Hoodies.tsx
@@ -1,11 +1,27 @@
+import { Button } from '@mui/material';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { ClothesCard } from '../../shared/ClothesCard/ClothesCard';
+import './Hoodies.scss'
 
 export const Hoodies = () => {
+    const items = [
+        { size: 'S', price: 2000, name: "Худи №1" },
+        { size: 'M', price: 2000, name: "Худи №2" },
+        { size: 'L', price: 2000, name: "Худи №3" },
+        { size: 'S', price: 2000, name: "Худи №4" },
+        { size: 'M', price: 2000, name: "Худи №5" },
+        { size: 'M', price: 2000, name: "Худи №6" },
+        { size: 'S', price: 2000, name: "Худи №7" },
+        { size: 'M', price: 2000, name: "Худи №8" },
+        { size: 'M', price: 2000, name: "Худи №9" }];
     return (
         <>
-            <h1>Hoodies</h1>
-            <Link to="/">go back</Link><br/>
+            <Button variant="text" href='/'> Назад</Button>
+            <div className='parent_hoodie'>
+                {items.map((i, index) =>
+                    <ClothesCard name={i.name} price={i.price} size={i.size} num={String(index + 1)} />
+                )}
+            </div>
         </>
     );
 };
---
+++ src/shared/ClothesCard/ClothesCard.tsx
@@ -0,0 +1,38 @@
+import React, {FC} from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+interface ClothesCardProps {
+    num: string,
+    name: string,
+    size: string,
+    price: number
+}
+
+export const ClothesCard: FC<ClothesCardProps> = ({name, size, price, num}) => {
+  return (
+    <div className={`hoodie${num}`}>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="300"
+          image={require("../static/hoodie.jpeg")}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name} {size} {price}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Худи удобное красивое классное
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </div>
+  );
+}
